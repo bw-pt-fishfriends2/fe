99040f7727ff944fef4c6a68870831fbd48b22ea
Christopher Lien <cnlien@gmail.com>
2019-12-20 14:58:43 -0500

    Removed routes from Nav and added them to App.js. Added Dashboard styling.

--- fish-friends/src/App.js
+++ fish-friends/src/App.js
@@ -1,18 +1,31 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, } from 'react-router-dom';
 
 import LoginForm from './components/LoginForm';
 import Navigation from './components/Navigation';
-
+import Dashboard from './components/Dashboard';
+import FindALocation from './components/FindALocation';
+import CreateAccount from './components/CreateAccount';
 import './App.css';
 
 function App() {
   return (
-
     
     <div className="App">
       <Navigation />
-      <LoginForm />
+
+      <Route exact path="/" component={LoginForm} />
+      
+      { /* 
+      THIS WILL BE A PRIVATE ROUTE
+      <PrivateRoute exact path="dashboard" component={Dashboard} />
+      */}
+
+      <Route path="/dashboard" component={Dashboard} />
+      <Route path="/fish-finder" component={FindALocation} />
+      <Route path="/register" component={CreateAccount} />
     </div>
+
   );
 }
 
--- fish-friends/src/components/Navigation.js
+++ fish-friends/src/components/Navigation.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import { Nav, NavItem, Navbar, NavbarBrand } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import { Nav, NavItem, Navbar } from 'reactstrap';
 
 import './Navigation.scss';
-
-import Dashboard from "./Dashboard";
-import CreateAccount from "./CreateAccount";
-
 import HorizontalLogo from '../images/FishFriendsLogo_Horizontal.svg';
 
 const Navigation = () => {
@@ -14,15 +10,13 @@ const Navigation = () => {
   return (
     <div>
       <Navbar>
-      <img className="nav-logo" src={HorizontalLogo} alt="Navigation Logo" />
-      <Nav>
-        <NavItem><Link to="/">Home</Link></NavItem>
-        <NavItem><Link to="/dashboard">Dashboard</Link></NavItem>
-        <NavItem><Link to="/register">Create Account</Link></NavItem>
-      </Nav>
+        <img className="nav-logo" src={HorizontalLogo} alt="Navigation Logo" />
+        <Nav>
+          <NavItem><Link to="/">Home</Link></NavItem>
+          <NavItem><Link to="/dashboard">Dashboard</Link></NavItem>
+          <NavItem><Link to="/register">Create Account</Link></NavItem>
+        </Nav>
       </Navbar>
-      <Route path="/dashboard" component={Dashboard} />
-      <Route path="/register" component={CreateAccount} />
     </div>
   )
 }
