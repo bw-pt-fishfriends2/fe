20d0b69f74618ed0b90e8d08508496d57baabae9
Christopher Lien <cnlien@gmail.com>
2019-12-19 21:46:03 -0500

    File and Component Structure

---
+++ fish-friends/src/components/Navigation.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Navigation =()=> {
+
+  return(
+    <div>
+      <h1>Navigation</h1>
+      {
+      /* 
+        <Link to="Dashboard.js">Home</Link>
+        <Link to="CreateAccount.js">Create Account</Link>
+      */
+      }
+    </div>
+  )
+}
+
+export default Navigation;
