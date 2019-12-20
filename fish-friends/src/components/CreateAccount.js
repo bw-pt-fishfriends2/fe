8e3b11ad9195e65d4c5594b852bc8b0a59131841
Nicole Nolla <22096240+sicklesium@users.noreply.github.com>
2019-12-19 22:17:09 -0500

    Navigation routed and working as intended.

---
+++ fish-friends/src/components/CreateAccount.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CreateAccount = () => {
+
+    return (
+        <div>
+            <h1>Create an Account</h1>
+        </div>
+    )
+}
+
+export default CreateAccount;
--- fish-friends/src/components/Navigation.js
+++ fish-friends/src/components/Navigation.js
@@ -1,16 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-const Navigation =()=> {
+import Dashboard from "./Dashboard";
+import CreateAccount from "./CreateAccount";
 
-  return(
+const Navigation = () => {
+
+  return (
     <div>
       <h1>Navigation</h1>
       {
-      /* 
-        <Link to="Dashboard.js">Home</Link>
-        <Link to="CreateAccount.js">Create Account</Link>
-      */
+        /* 
+          <Link to="Dashboard.js">Home</Link>
+          <Link to="CreateAccount.js">Create Account</Link>
+        */
       }
+      <Link to="/">Home</Link>
+      <Link to="/dashboard">Dashboard</Link>
+      <Link to="/register">Create Account</Link>
+
+      <Route path="/dashboard" component={Dashboard} />
+      <Route path="/register" component={CreateAccount} />
     </div>
   )
 }
