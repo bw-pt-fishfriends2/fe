3d4cd62c656cad7293ed68b742af923021ae72ac
Lambda_School_Loaner_212 <lambda_school_loaner_212@Lambda-School-Loaner-212s-MacBook-Pro.local>
2019-12-21 12:53:09 -0800

    geolocation almost complete

--- fish-friends/src/components/GeoLocation.js
+++ fish-friends/src/components/GeoLocation.js
@@ -1,14 +1,55 @@
 import { NativeModules, DeviceEventEmitter } from 'react-native';
 import Config from 'react-native-config';
+import Geocoder from 'react-native-geocoding';
+const { Geolocation } = 'react-native-geolocation-service';
+import Reacct, { Component } from 'react';
+import { Styesheet, Text, View } from 'react-native';
 
-const { GeoLocation } = NativeModules;
+Geocoder.init('AIzaSyB6z99ZdNTCK0s4Z24_skvbDu4EZRjKL0I');
+Geocoder.from(41.89, 12.49)
+    .then(json => {
+       
+        console.log(addressComponent);
+    })
+    .catch(error => 
+        consolse.warn(error)
+        );
 
-class GeoLocation {
-    constructor(token, user_id) {
-        this.state= null
+
+export default class GeoLocation extends Component{
+    constructor() {
+        super()
+        this.state= {
+            latitude: 0,
+            longitude: 0,
+            error: null,
+            Address: null
+        }
     }
-    start(dispatch, nextState){
-        this.dispatch = dispatch
-        const token = get()
+    async componentDidMount(){
+        Geolocation.getCurrentPosition(
+            (position) => {
+                console.log(position);
+                this.setState = ({
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude,
+                });
+                Geocoder.from(positon.coords.latitude, position.coords.longitude)
+                    .then(json => {
+                        const addressComponent = json.results[0].address_components[0];
+                        this.setState({
+                            Address: addressComponent
+                        })
+                    })
+            },
+            (error) => {
+                console.log(error.code, error.message);
+            },
+            {
+                enableHighAccuracy: false,
+                timeout: 10000,
+                maximumAge: 100000
+            }
+        )
     }
 }
