a827af9701e27b4db38152ebc3e1966af4f7bd29
Lambda_School_Loaner_212 <lambda_school_loaner_212@Lambda-School-Loaner-212s-MacBook-Pro.local>
2019-12-22 17:54:41 -0800

    Cleared import errors and added components to use for when we are confident in doing our stretch goals

--- fish-friends/src/App.js
+++ fish-friends/src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
-
+import Navigation from './components/Navigation';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import './App.css';
-
+import LoginForm from './components/LoginForm';
+import Dashboard from './components/Dashboard';
+import FindALocation from './components/FindALocation';
+import CreateAccount from './components/CreateAccount';
 
 function App() {
   return (
--- fish-friends/src/components/tweetStory.js
+++ fish-friends/src/components/tweetStory.js
@@ -3,3 +3,6 @@ const TweetStory = id => {
     const storyElem = document.getElementById(`${this.StaticRange.selectedStory}`);
     const story = storyElem ? storyElem.textContent : "I just made a cool story with dev-libs!"
 }
+
+
+//----https://github.com/bw-dev-libs/Front-End/pull/15/commits/90d2226b57ff9ff4d0e0976dc92dbbc986886c31
