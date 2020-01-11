986d0db8680b2cb97e99e9ba28fb84f413216860
CryptoN999 <nicholasgallucci4@gmail.com>
2020-01-10 17:17:59 -0800

    working on buttons to updated/delete logs

--- fish-friends/src/components/DashboardLogCard.js
+++ fish-friends/src/components/DashboardLogCard.js
@@ -3,13 +3,13 @@ import axiosWithAuth from '../utils/AxiosWithAuth';
 
 import { UserContext } from '../contexts/UserContext';
 
-import { Card, Media, CardSubtitle, CardText, CardImg } from 'reactstrap'
+import { Card, Media, CardSubtitle, Button, CardText, CardImg } from 'reactstrap'
 import './styles/Dashboard.scss';
 
 import AccessTimeIcon from '@material-ui/icons/AccessTime';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
 
-export const DashboardLogCard = () => {
+export const DashboardLogCard = (props) => {
 
   // const { user } = useContext(UserContext);
   // console.log("DashboardLogCard.js User: ", user);
@@ -18,13 +18,33 @@ export const DashboardLogCard = () => {
 
   useEffect(() => {
     axiosWithAuth()
-      .get("logRoute/all-logs/")
+      .get(`logRoute/all-logs/`)
       .then(res => {
         setLog(res.data);
       })
       .catch(err=> console.log(err.message));
   }, []);
-  
+
+  const cancel = e => {
+    e.preventDefault();
+    props.history.push(`/`);
+  };
+  const editEvent = {
+    // species: ,
+    // baitused: ,
+    // time: -,
+    // date: -,
+    // locaion: 
+  };
+  const deleteEvent = e => {
+    e.preventDefault();
+    axiosWithAuth()
+      .delete('logRoute/${log.id}')
+      .then(res => {
+        log.history.push(`/Home`);
+      })
+      .catch(err => console.error(err));
+  };
   
   return (
     <Card className="db-logCard">
@@ -54,6 +74,12 @@ export const DashboardLogCard = () => {
         <CardText className="db-logCardMeta">
           <LocationOnIcon style={{fontSize: 15}}/> {log.facilityName}
         </CardText>
+        <Button>
+          Update
+        </Button>
+        <Button className="logDeleteButton" onClick={e => deleteEvent(e)}>
+          Delete
+        </Button>
       </div>
 
       <div className="db-logCardInfo">
